7c6115b5456e9d4accbb6b34ea8aa1d527e55068
0xgiovatrc <giovanniturchetto1999@gmail.com>
2023-12-24 21:22:59 +0100

    Update files - create base structure

---
+++ assets/script/script.js
@@ -0,0 +1,46 @@
+const timer = document.querySelector(".timer");
+let myNumber = 0;
+
+document.querySelector(".plus-button").addEventListener("click", function() {
+    if (myNumber >= 0) {
+        myNumber++;
+        document.querySelector(".counter").innerHTML = myNumber;
+    };
+    return
+});
+
+document.querySelector(".minus-button").addEventListener("click", function() {
+    if (myNumber > 0) {
+        myNumber--;
+        document.querySelector(".counter").innerHTML = myNumber;
+    };
+    return
+});
+
+document.querySelector(".target-number-generator").addEventListener("click", function() {
+    const targetNumber1= getRandomInt(15, 21);
+    document.querySelector(".target-number-container")
+        .innerHTML = `
+            <p>Target Number: </p>
+            <p class="target-number">${targetNumber1}</p>
+        `;
+    let timeCentiSeconds = 600;
+    const timerCountDown = setInterval(() => {
+        timeCentiSeconds--;
+        displayTime(timeCentiSeconds);
+        if (timeCentiSeconds === 0) {
+            clearInterval(timerCountDown);
+        }}, (1000 / 60));
+});
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function displayTime(centiSeconds) {
+    const sec = Math.floor(centiSeconds / 60);
+    const centiSec = Math.floor(centiSeconds % 60);
+    timer.innerHTML = `${sec < 10 ? "0" : ""}${sec}:${centiSec < 10 ? "0" : ""}${centiSec}`;
+}
